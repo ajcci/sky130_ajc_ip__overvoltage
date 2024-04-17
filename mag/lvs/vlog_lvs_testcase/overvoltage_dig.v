894f10988c35bdf3679154d3ed56d603f9620f8f
Robin Matthew Tsang <robin.tsang@ajacci.com>
2024-04-16 22:32:12 -0500

    debug LVS

--- mag/lvs/vlog_lvs_testcase/overvoltage_dig.v
+++ mag/lvs/vlog_lvs_testcase/overvoltage_dig.v
@@ -1,13 +1,14 @@
 module overvoltage_dig (
-input logic VPWR, 
-input logic VGND, 
-input logic otrip, 
-output logic otrip_decoded
+input VPWR, 
+input VGND, 
+input otrip, 
+output otrip_decoded
 );
-//input VPWR;
-//input VGND;
-//input otrip;
-//output otrip_decoded;
+
+wire VPWR;
+wire VGND;
+wire otrip;
+wire otrip_decoded;
 
  sky130_fd_sc_hd__buf_2 output0 (.A(otrip),
     .VGND(VGND),
@@ -15,7 +16,5 @@ output logic otrip_decoded
     .VPB(VPWR),
     .VPWR(VPWR),
     .X(otrip_decoded));
-
-//sky130_fd_sc_hd__buf_2 output0 (otrip,VGND,VGND,VPWR,VPWR,otrip_decoded);
 
 endmodule
