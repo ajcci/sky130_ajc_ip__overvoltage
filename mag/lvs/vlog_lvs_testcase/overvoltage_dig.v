7da9b67180c4014c9bb9b6b09818883bf26010ad
Robin Matthew Tsang <robin.tsang@ajacci.com>
2024-04-16 21:39:50 -0500

    test case for Verilog LVS

--- mag/lvs/vlog_lvs_testcase/overvoltage_dig.v
+++ mag/lvs/vlog_lvs_testcase/overvoltage_dig.v
@@ -1,16 +1,21 @@
-module overvoltage_dig (VPWR,VGND,otrip,otrip_decoded);
-input VPWR;
-input VGND;
-input otrip;
-output otrip_decoded;
+module overvoltage_dig (
+input logic VPWR, 
+input logic VGND, 
+input logic otrip, 
+output logic otrip_decoded
+);
+//input VPWR;
+//input VGND;
+//input otrip;
+//output otrip_decoded;
 
-// sky130_fd_sc_hd__buf_2 output0 (.A(otrip0),
-//    .VGND(VGND),
-//    .VNB(VGND),
-//    .VPB(VPWR),
-//    .VPWR(VPWR),
-//    .X(otrip_decoded));
+ sky130_fd_sc_hd__buf_2 output0 (.A(otrip),
+    .VGND(VGND),
+    .VNB(VGND),
+    .VPB(VPWR),
+    .VPWR(VPWR),
+    .X(otrip_decoded));
 
-sky130_fd_sc_hd__buf_2 output0 (otrip,VGND,VGND,VPWR,VPWR,otrip_decoded);
+//sky130_fd_sc_hd__buf_2 output0 (otrip,VGND,VGND,VPWR,VPWR,otrip_decoded);
 
 endmodule
